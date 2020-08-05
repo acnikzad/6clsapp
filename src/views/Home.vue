840466bc936c20f5d98545544ede397906b735a0
“Aria <a.c.nikzad@gmail.com>
2020-08-04 18:55:31 -0700

    getting these links to work

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,7 @@
                 <span class="image">
                   <img src="images/cls/20181009_124907.jpg" alt="" />
                 </span>
-                <a href="concrete">
+                <a href="/concrete">
                   <h2>Concrete</h2>
                   <div class="content">
                     <p></p>
