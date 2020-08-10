08ef11970b65cc7baa29d31ed1f86ac43cb796f8
“Aria <a.c.nikzad@gmail.com>
2020-08-10 14:57:56 -0700

    omg xtz

--- src/views/Repair.vue
+++ src/views/Repair.vue
@@ -8,11 +8,12 @@
         <h1>Our Repair and Restoration Services</h1>
       </header>
       <div class="row gtr-200">
-        <div class="col-8 col-12-medium">
-          <span class="image fit"><img src="images/cls/20200401_173206.jpg" alt="" /></span>
+        <div class="col-7 col-12-medium">
+          <span class="image fit"><img src="images/cls/49433534276_a0015e47f5_o.jpg" alt="" /></span>
+          <span class="image fit"><img src="images/cls/49471210098_50e66f7caf_o.jpg" alt="" /></span>
         </div>
-        <div class="col-4 col-12-medium">
-          <h3>Patios</h3>
+        <div class="col-5 col-12-medium">
+          <h3>Repair</h3>
             <p><i>Do you need to replace or repair concrete? We can help!</i></p>
             <p>We can help you achieve the look you want, going that extra mile to ensure that you are satisfied. Whether it’s adding or expanding a driveway, walkway, sidewalk, steps, patio, footings, mow strip, pads or epoxy flooring, we are your local concrete flat work specialist.</p>
         </div>
@@ -25,7 +26,8 @@
             <p>Upgrade your home with new or extended concrete installations. We can bring your ideas to life. Concrete Landscape Services specializes in Walkways, Driveways, Patios, Pool Decks, RV Pads, Steps, Footings and Mow Strips. Installing concrete for YOU, the way you want it, that’s what we do BEST!!!</p>
         </div>
         <div class="col-8 col-12-medium">
-          <span class="image fit"><img src="images/cls/Broom stain driveway.jpg" alt="" /></span>
+          <span class="image fit"><img src="images/cls/49622415613_820c906c0b_o.jpg" alt="" /></span>
+          <span class="image fit"><img src="images/cls/49657107738_473a1b8ba9_o.jpg" alt="" /></span>
         </div>
       </div>
       <hr>
